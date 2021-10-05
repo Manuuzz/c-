ba07e2b055df446844f45a7277a39248587764c0
Manuuzz <90182097+Manuuzz@users.noreply.github.com>
2021-10-05 09:11:28 +0300

    Add files via upload

---
+++ chap2tp2/exo2/exo2/exo2.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include<stdio.h>
+#include<conio.h>
+#include <tchar.h>
+
+int main()
+{
+	float t[3][4];
+	float m[3];
+	int i, moyenne;
+
+}
+
